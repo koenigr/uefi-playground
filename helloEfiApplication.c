3e2552e8756680f03ba226b1aa7597c2a6fb2ef9
Regina König <koenig_regina@arcor.de>
2023-07-24 18:29:00 +0200

    Makefile

--- helloEfiApplication.c
+++ helloEfiApplication.c
@@ -3,7 +3,7 @@
 
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	InitializeLib(ImageHandle, SystemTable);
-	Print(L"Hellom world!\n");
+	Print(L"Hello world!\n");
 
-	return EFI_SUCCES;
+	return EFI_SUCCESS;
 }
