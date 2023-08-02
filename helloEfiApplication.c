34b95e03cea386c2f1a3e3ad1266fefa25da89c1
Regina König <koenig_regina@arcor.de>
2023-08-02 12:21:31 +0200

    Change hello text

--- helloEfiApplication.c
+++ helloEfiApplication.c
@@ -3,7 +3,7 @@
 
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	InitializeLib(ImageHandle, SystemTable);
-	Print(L"Hello world!\n");
+	Print(L"Hello Efi Application!\n");
 
 	return EFI_SUCCESS;
 }
