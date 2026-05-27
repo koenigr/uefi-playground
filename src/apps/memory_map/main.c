acc31c7a0f4621ea72b16d467f39f115e681e3c1
Regina König <10077142+koenigr@users.noreply.github.com>
2026-05-27 20:42:34 +0200

    Allocate Memory for MemoryMap

--- src/apps/memory_map/main.c
+++ src/apps/memory_map/main.c
@@ -11,5 +11,57 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
 	Print(L"UEFI Revision: %d\n", SystemTable->Hdr.Revision);
 	Print(L"BootServices: %p\n", SystemTable->BootServices);
 
+	EFI_MEMORY_DESCRIPTOR *MemoryMap = NULL;
+	UINTN MemoryMapSize = 0;
+	UINTN MapKey;
+	UINTN DescriptorSize;
+	UINT32 DescriptorVersion;
+	EFI_STATUS Status;
+
+	Status = SystemTable->BootServices->GetMemoryMap(
+		&MemoryMapSize,
+		NULL,
+		&MapKey,
+		&DescriptorSize,
+		&DescriptorVersion
+	);
+
+	if (Status != EFI_BUFFER_TOO_SMALL) {
+		Print(L"GetMemoryMap failed\n");
+		return Status;
+	}
+
+	Print(L"MemoryMapSize: %d\n", MemoryMapSize);
+
+	Status = SystemTable->BootServices->AllocatePool(
+		EfiLoaderData,
+		MemoryMapSize,
+		(void**)&MemoryMap
+	);
+
+	Status = SystemTable->BootServices->GetMemoryMap(
+		&MemoryMapSize,
+		MemoryMap,
+		&MapKey,
+		&DescriptorSize,
+		&DescriptorVersion
+	);
+
+	Print(L"MemoryMapSize: %d\n", MemoryMapSize);
+
+	for (
+    	EFI_MEMORY_DESCRIPTOR *Desc = MemoryMap;
+    	(UINT8*)Desc < (UINT8*)MemoryMap + MemoryMapSize;
+    	Desc = (EFI_MEMORY_DESCRIPTOR*)((UINT8*)Desc + DescriptorSize)
+	) {
+		Print(L"Type: %d Pages: %lu\n", Desc->Type, Desc->NumberOfPages);
+	}
+
+	UINTN Count = MemoryMapSize / DescriptorSize;
+
+	Print(L"MemoryMapSize: %lx\n", MemoryMapSize);
+	Print(L"DescriptorSize: %d\n", DescriptorSize);
+	Print(L"MapKey: %d\n", MapKey);
+
 	return EFI_SUCCESS;
 }
