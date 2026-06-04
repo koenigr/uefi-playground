681a1afa973ddb12a7d02b88154f48a61412a466
koenigr <10077142+koenigr@users.noreply.github.com>
2026-06-04 20:42:36 +0200

    Apply LocateProtocol (#41)

--- src/apps/filesystem/main.c
+++ src/apps/filesystem/main.c
@@ -1,9 +1,29 @@
 #include <efi.h>
 #include <efilib.h>
 
+
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	InitializeLib(ImageHandle, SystemTable);
 	Print(L"Filesystem Application!\n");
+
+	EFI_STATUS Status;
+	EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *Fs = NULL;
+	EFI_BOOT_SERVICES *BootServices = SystemTable->BootServices;
+
+	Status = uefi_call_wrapper(
+		BootServices->LocateProtocol,
+		3,
+		&gEfiSimpleFileSystemProtocolGuid,
+		NULL,
+		(VOID**)&Fs
+	);
+
+	if (EFI_ERROR(Status)) {
+		Print(L"Fehler beim Öffnen des Volumes: %r\n", Status);
+		return Status;
+	}
+
+	Print(L"Located Simple File System Protocol\n");
 
 	return EFI_SUCCESS;
 }
