5f66e9aa1d0be5ba89c8475e82bc0880ae4921a9
koenigr <10077142+koenigr@users.noreply.github.com>
2026-06-08 22:20:49 +0200

    Open root Volume (#42)

--- src/apps/filesystem/main.c
+++ src/apps/filesystem/main.c
@@ -19,11 +19,27 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
 	);
 
 	if (EFI_ERROR(Status)) {
-		Print(L"Fehler beim Öffnen des Volumes: %r\n", Status);
+		Print(L"Failed to locate Simple File System Protocl: %r\n", Status);
 		return Status;
 	}
 
 	Print(L"Located Simple File System Protocol\n");
+
+	EFI_FILE_PROTOCOL *Root = NULL;
+
+	Status = uefi_call_wrapper(
+		Fs->OpenVolume,
+		2,
+		Fs,
+		&Root
+	);
+
+	if (EFI_ERROR(Status)) {
+		Print(L"Error while opening root volume: %r\n", Status);
+		return Status;
+	}
+
+	Print(L"Opened root volume\n");
 
 	return EFI_SUCCESS;
 }
