cd79700af33586c938b5e6df65a0b3391484df83
Regina König <10077142+koenigr@users.noreply.github.com>
2026-06-09 21:34:41 +0200

    Read first directory entry

--- src/apps/filesystem/main.c
+++ src/apps/filesystem/main.c
@@ -41,5 +41,75 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
 
 	Print(L"Opened root volume\n");
 
+	VOID *Buffer = NULL;
+	UINTN BufferSize = 1024;
+
+	Status = uefi_call_wrapper(
+		SystemTable->BootServices->AllocatePool,
+		3,
+		EfiLoaderData,
+		BufferSize,
+		&Buffer
+	);
+
+	if (EFI_ERROR(Status)) {
+		Print(L"AllocatePool failed: %r\n", Status);
+		return Status;
+	}
+
+	Status = uefi_call_wrapper(
+		Root->Read,
+		3,
+		Root,
+		&BufferSize,
+		Buffer
+	);
+
+	if (EFI_ERROR(Status) && Status != EFI_BUFFER_TOO_SMALL) {
+		Print(L"Read failed: %r\n", Status);
+		return Status;
+	}
+
+	if (Status == EFI_BUFFER_TOO_SMALL) {
+
+		FreePool(Buffer);
+		Buffer = NULL;
+
+		Status = uefi_call_wrapper(
+			BootServices->AllocatePool,
+			3,
+			EfiLoaderData,
+			BufferSize,
+			&Buffer
+		);
+
+		if (EFI_ERROR(Status))
+			return Status;
+
+		Status = uefi_call_wrapper(
+			Root->Read,
+			3,
+			Root,
+			&BufferSize,
+			Buffer
+		);
+
+		if (EFI_ERROR(Status)) {
+			Print(L"Read failed: %r\n", Status);
+			return Status;
+		}
+
+	}
+
+	if (BufferSize == 0) {
+		Print(L"No directory entries found\n");
+		return EFI_SUCCESS;
+	}
+
+	EFI_FILE_INFO *FileInfo = (EFI_FILE_INFO*)Buffer;
+	Print(L"Entry: %s\n", FileInfo->FileName);
+
+	FreePool(Buffer);
+
 	return EFI_SUCCESS;
 }
