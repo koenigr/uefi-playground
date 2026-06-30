be3a59d04cbdaf0f9c67a8a812aa3422851cc45d
koenigr <10077142+koenigr@users.noreply.github.com>
2026-06-30 12:56:30 +0200

    Locate Graphics Output Protocol (#60)

--- src/apps/graphics/main.c
+++ src/apps/graphics/main.c
@@ -5,5 +5,22 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
 	InitializeLib(ImageHandle, SystemTable);
 	Print(L"Graphics Application!\n");
 
+	EFI_STATUS Status;
+	EFI_GRAPHICS_OUTPUT_PROTOCOL *gop = NULL;
+	EFI_BOOT_SERVICES *BootServices = SystemTable->BootServices;
+
+	Status = uefi_call_wrapper(
+		BootServices->LocateProtocol,
+		3,
+		&gEfiGraphicsOutputProtocolGuid,
+		NULL,
+		(VOID**)&gop
+	);
+
+	if (EFI_ERROR(Status)) {
+		Print(L"Failed to load Graphics Output Protocol: %r\n", Status);
+		return Status;
+	}
+
 	return EFI_SUCCESS;
 }
